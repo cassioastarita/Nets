d2378b33027686439fbae72ba7da65d97debaaf0
Jcirona <jcirona@hotmail.com>
2022-03-16 14:37:41 +1100

    add_court complete

---
+++ models/court.js
@@ -0,0 +1,24 @@
+const db = require('../db/db')
+
+const Courts = {
+    findAll: () => {
+        const sql = "SELECT * FROM court_info"
+
+        return db   
+            .query(sql)
+            .then(dbRes => dbRes.rows)
+    },
+    create: (courtName, net, toilet, water, parking, imgUrl) => {
+        const sql =`
+        INSERT INTO court_info(court_name, net, toilet, water, parking, img_url)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING *
+        `
+
+        return db
+            .query(sql, [courtName, net, toilet, water, parking, imgUrl])
+            .then(dbRes => dbRes.rows[0])
+    }
+}
+
+module.exports = Courts
