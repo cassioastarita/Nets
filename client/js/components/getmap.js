890cc429aaec1a9873f03338f3caf742aace38af
Cassio Astarita <cassioastarita@gmail.com>
2022-03-17 16:25:31 +1100

    css

--- client/js/components/getmap.js
+++ client/js/components/getmap.js
@@ -1,21 +1,24 @@
-function GetMap() {
+function renderGetMap() {
+  document.querySelector('#myMap').innerHTML = `
+    <section class="getmap">
+  
+    </section>
+  `
+  // how to show the map in the page
+}
+
+function getMap() {
   var map = new Microsoft.Maps.Map('#myMap', {
       credentials: 'Your Bing Maps Key',
       center: new Microsoft.Maps.Location(47.6149, -122.1941)
   });
   var center = map.getCenter();
   //Create custom Pushpin
-
-
-
   var pin = new Microsoft.Maps.Pushpin(center, {
       title: 'Microsoft',
       subTitle: 'City Center',
       text: '1'
   });
   //Add the pushpin to the map
-
-
-  
   map.entities.push(pin);
 }
