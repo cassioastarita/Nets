6bd3d6ddf5c129226c8a9cfcdbd06085546e8258
Jcirona <jcirona@hotmail.com>
2022-03-17 12:44:21 +1100

    Added radio group for boolean fields

--- client/js/components/add_courts.js
+++ client/js/components/add_courts.js
@@ -7,25 +7,49 @@ function renderAddCourt() {
               <input type="text" name="courtName" />
             </fieldset>
 
-            <fieldset>
-              <label for="">Fenced Court: </label><br />
-              <input type="text" name="net" />
-            </fieldset>
+            <div class="radio-group">
+            <label for="">Netted Court?</label><br />
+            <input type="radio" name="net" id="net_yes" value="yes">
+            <label for="yes">Yes</lablel>
+            </div>
 
-            <fieldset>
-            <label for="">Toilets: </label><br />
-            <input type="text" name="toilet" />
-            </fieldset>
+            <div class="radio-group">
+            <input type="radio" name="net" id="net_no" value="no">
+            <label for="no">No</label>
+            </div>
+             
+            <div class="radio-group">
+            <label for="">Toilets?</label><br />
+            <input type="radio" name="toilet" id="toilet_yes" value="yes">
+            <label for="yes">Yes</lablel>
+            </div>
 
-            <fieldset>
-            <label for="">Drink Taps: </label><br />
-            <input type="text" name="water" />
-            </fieldset>
+            <div class="radio-group">
+            <input type="radio" name="toilet" id="toilet_no" value="no">
+            <label for="no">No</label>
+            </div>
+             
+            <div class="radio-group">
+            <label for="">Drink Taps?</label><br />
+            <input type="radio" name="water" id="water_yes" value="yes">
+            <label for="yes">Yes</lablel>
+            </div>
 
-            <fieldset>
-            <label for="">Parking: </label><br />
-            <input type="text" name="parking" />
-          </fieldset>
+            <div class="radio-group">
+            <input type="radio" name="water" id="water_no" value="no">
+            <label for="no">No</label>
+            </div>
+             
+            <div class="radio-group">
+            <label for="">Parking?</label><br />
+            <input type="radio" name="parking" id="parking_yes" value="yes">
+            <label for="yes">Yes</lablel>
+            </div>
+
+            <div class="radio-group">
+            <input type="radio" name="parking" id="parking_no" value="no">
+            <label for="no">No</label>
+            </div>
 
             <fieldset>
             <label for="">Image: </label><br />
@@ -34,16 +58,42 @@ function renderAddCourt() {
           
             <button>Add Court</button>
           </form>
-        </section>`;
+        </section>`
 }
 
-function createCourt(event) {
+function addCourt(event) {
     event.preventDefault();
     const form = event.target;
     const data = Object.fromEntries(new FormData(form));
+    if (data.net == 'yes') {
+      data.net = true
+    } else {
+      data.net = false
+    }
+
+    if (data.toilet == 'yes') {
+      data.toilet = true
+    } else {
+      data.toilet = false
+    }
+
+    if (data.water == 'yes') {
+      data.water = true
+    } else {
+      data.water = false
+    }
+
+    if (data.parking == 'yes') {
+      data.parking = true
+    } else {
+      data.parking = false
+    }
+
+    console.log(data)
     axios.post('/api/courts', data)
       .then(res => res.data)
       .then(newCourt => state.courts.push(newCourt))
-      .then(() => renderCourtList())
+
+      // .then(() => renderCourtList())
 
 }
